6c74d63b86a20d47a17e2d7faad51579ee69a2f0
miku86 <miku86coding@gmail.com>
2019-02-11 13:30:42 +0100

    add german translation of in-code text snippets

--- content/home/examples/a-component-using-external-plugins.js
+++ content/home/examples/a-component-using-external-plugins.js
@@ -2,7 +2,7 @@ class MarkdownEditor extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = { value: 'Hello, **world**!' };
+    this.state = { value: 'Hallo, **Welt**!' };
   }
 
   handleChange(e) {
@@ -17,16 +17,16 @@ class MarkdownEditor extends React.Component {
   render() {
     return (
       <div className="MarkdownEditor">
-        <h3>Input</h3>
+        <h3>Eingabe</h3>
         <label htmlFor="markdown-content">
-          Enter some markdown
+          Gib Markdown Code ein
         </label>
         <textarea
           id="markdown-content"
           onChange={this.handleChange}
           defaultValue={this.state.value}
         />
-        <h3>Output</h3>
+        <h3>Ausgabe</h3>
         <div
           className="content"
           dangerouslySetInnerHTML={this.getRawMarkup()}
