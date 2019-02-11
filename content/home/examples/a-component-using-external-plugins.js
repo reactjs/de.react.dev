732bdf1fc0ad78882d7312d6a370a7c963d79335
Chris Ben <chris.ben9@gmail.com>
2019-02-11 20:09:03 +0100

    Update content/home/examples/a-component-using-external-plugins.js
    Co-Authored-By: miku86 <miku86coding@gmail.com>

--- content/home/examples/a-component-using-external-plugins.js
+++ content/home/examples/a-component-using-external-plugins.js
@@ -2,7 +2,7 @@ class MarkdownEditor extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = { value: 'Hallo, **Welt**!' };
+    this.state = { value: 'Hallo **Welt**!' };
   }
 
   handleChange(e) {
