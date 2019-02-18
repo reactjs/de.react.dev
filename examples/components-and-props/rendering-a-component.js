655c888b590642ff04298028a45566e834cf1807
Chris Ben <chris.ben9@gmail.com>
2019-02-18 11:07:59 +0100

    Update examples/components-and-props/rendering-a-component.js
    Co-Authored-By: ph1p <phil@capshake.com>

--- examples/components-and-props/rendering-a-component.js
+++ examples/components-and-props/rendering-a-component.js
@@ -2,5 +2,5 @@ function Welcome(props) {
   return <h1>Hallo {props.name}</h1>;
 }
 
-const element = <Welcome name="Sara" />;
+const element = <Welcome name="Sarah" />;
 ReactDOM.render(element, document.getElementById('root'));
