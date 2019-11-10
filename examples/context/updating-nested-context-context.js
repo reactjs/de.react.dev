e58729556280659eda377043aaaf230a67424da9
Tamara <t_und@hotmail.com>
2019-11-10 13:32:46 +0000

    Update examples/context/updating-nested-context-context.js
    Co-Authored-By: Phil <phil@capshake.com>

--- examples/context/updating-nested-context-context.js
+++ examples/context/updating-nested-context-context.js
@@ -1,4 +1,4 @@
-// Stelle sicher, dass die Form des Default-Wertes, welcher an createContext
+// Stelle sicher, dass die Form des Standardwertes, welcher an createContext
 // überreicht wird mit der Form die der Konsument erwartet, übereinstimmt!
 // highlight-range{2-3}
 export const ThemeContext = React.createContext({
